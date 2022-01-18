8fc6c28bc4e07db3f96b1b29e06732765c922412
tomiprasetyo <tomi.prasetyo95@gmail.com>
2022-01-18 21:03:02 +0700

    Belajar ReactJS - Stateless Component/Functional Component & Stateful Component/Class Component

---
+++ src/component/FunctionalComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./FunctionalComponent.css";
+
+const FunctionalComponent = () => {
+    return (
+        <p className='text-p'>Hello Stateless/Functional Component</p>
+    )
+}
+
+export default FunctionalComponent;
---
+++ src/container/ClassComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class ClassComponent extends React.Component {
+    render() {
+        return <p>Stateful Component</p>
+    }
+}
+
+export default ClassComponent
