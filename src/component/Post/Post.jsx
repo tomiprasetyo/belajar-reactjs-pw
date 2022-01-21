397cd24e11c69364a982908aac48e08db1a45d99
tomiprasetyo <tomi.prasetyo95@gmail.com>
2022-01-21 18:40:55 +0700

    Interaksi dengan Backend, Pemanggilan dummy API dengan method GET menggunakan fetch & axios

--- src/component/Post/Post.jsx
+++ src/component/Post/Post.jsx
@@ -7,8 +7,8 @@ const Post = (props) => {
                 <img src="https://placeimg.com/200/150/tech" alt="dummyImage" />
             </div>
             <div className='content'>
-                <p className='title'>Title</p>
-                <p className='desc'>Body</p>
+                <p className='title'>{props.title}</p>
+                <p className='desc'>{props.desc}</p>
             </div>
         </div>
     )
--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -1,15 +1,39 @@
 import React, { Component, Fragment } from 'react';
 import Post from '../../component/Post/Post';
 import './BlogPost.css'
-
+import axios from 'axios'
 class BlogPost extends Component {
+    state = {
+        post: []
+    }
+
+    componentDidMount() {
+        // fetch('https://jsonplaceholder.typicode.com/posts')
+        // .then(response => response.json())
+        // .then(json => {
+        //     this.setState({
+        //         post: json
+        //     })
+        // })
+
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((result) => {
+            this.setState({
+                post: result.data
+            })
+        })
+    }
+
     render() {
         return (
             <Fragment>
                 <p className='section-title'>BlogPost</p>
-                <Post title='title' desc='desc' />
+                {
+                    this.state.post.map(post => {
+                        return <Post key={post.id} title={post.title} desc={post.body} />
+                    })
+                }
             </Fragment>
-
         )
     }
 }
